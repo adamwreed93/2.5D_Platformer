66fcf6ebee6fbf0df9aed062c31c600d371bcdf2
adamwreed93 <68203005+adamwreed93@users.noreply.github.com>
2021-06-16 15:45:38 -0700

    Added collectable coins along with coin variations that alot different values.

--- 2.5D_Platformer/Assets/Scripts/Player.cs
+++ 2.5D_Platformer/Assets/Scripts/Player.cs
@@ -10,18 +10,25 @@ public class Player : MonoBehaviour
     [SerializeField] private float _jumpHight = 15.0f;
     private float _yVelocity;
     private bool _canDoubleJump;
+    [SerializeField] private int _coins;
+    private UIManager _uiManager;
 
     void Start()
     {
         _controller = GetComponent<CharacterController>();
+        _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+
+        if (_uiManager == null)
+        {
+            Debug.Log("UI Manager is NULL!");
+        }
     }
 
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         Vector3 direction = new Vector3(horizontalInput, 0, 0);
-        Vector3 velocity = direction * _speed;
-        
+        Vector3 velocity = direction * _speed;  
     
         if (_controller.isGrounded)
         {
@@ -44,5 +51,22 @@ public class Player : MonoBehaviour
         velocity.y = _yVelocity;
 
         _controller.Move(velocity * Time.deltaTime);
+    }
+
+    public void AddCoins(int ID)
+    {
+        switch (ID)
+        {
+            case 0:
+                _coins += 1;
+                break;
+            case 1:
+                _coins += 5;
+                break;
+            case 2:
+                _coins += 10;
+                break;
+        }
+        _uiManager.UpdateCoinDisplay(_coins);
     }
 }
